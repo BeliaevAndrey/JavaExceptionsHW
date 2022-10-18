8e2d00db7cbc9b8a4d53aefc54f459be2c3ff5fe
Andrei Beliaev <abbot190414@gmail.com>
2022-10-18 17:25:36 +0300

    Exceptions, Homework 01: completed.

--- homework01/src/Task01.java
+++ homework01/src/Task01.java
@@ -1,2 +1,104 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task01 {
+
+    void obtainStackOverflow() {
+        /* Exception in thread "main" java.lang.StackOverflowError
+         */
+        Task01 inner = new Task01();
+        inner.obtainStackOverflow();
+    }
+
+    long factorial(long a) {
+        /* Exception in thread "main" java.lang.StackOverflowError
+         */
+        if (a == 1)
+            return 1;
+        return a * factorial(a + 1);    // An intentional mistake here to get StackOverflow
+    }
+
+    void obtainOutOfMemory() {
+        /* Exception in thread "main" java.lang.OutOfMemoryError:
+           Java heap space
+         */
+
+        ArrayList<String[]> list = new ArrayList<>();
+        while (true) {
+            list.add(new String[0]);
+        }
+    }
+
+    void obtainOutOfMemory2() {
+        /* Exception in thread "main" java.lang.OutOfMemoryError:
+           Requested array size exceeds VM limit
+         */
+        String[] s = new String[Integer.MAX_VALUE];
+    }
+
+    void obtainDivisionByZero() {
+        /* Exception in thread "main" java.lang.ArithmeticException:
+           / by zero
+        */
+        int a = 10 / 0;
+    }
+
+    String obtainNullPointerException(Integer[] array) {
+        int[] outArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            outArray[i] = array[i];
+        }
+        return Arrays.toString(outArray);
+    }
+
+    void obtainIllegalStateException() {
+        /* Exception in thread "main" java.lang.IllegalStateException:
+           Scanner closed
+        */
+        Scanner scn = new Scanner(System.in);
+        scn.close();
+        scn.nextLine();
+
+}
+
+    public static void main(String[] args) {
+        Task01 app = new Task01();
+
+//        app.obtainStackOverflow();    // 1
+
+//        app.factorial(5);   // (StackOverflow 2)
+
+//        app.obtainOutOfMemory();      // 2
+
+//        app.obtainOutOfMemory2();
+
+//        app.obtainDivisionByZero();   // 3
+
+
+        // NullPointerException Section // 4
+        /*
+
+        {
+            System.out.println(
+                    app.obtainNullPointerException(
+                            new Integer[]{1, 2, 3, 4, 5}));
+            // check if method works correctly at all
+
+
+            System.out.println(
+                    app.obtainNullPointerException(
+                            new Integer[]{1, 2, null, 4, 5}));
+            // intentionally incorrect call
+            //Output: Exception in thread "main" java.lang.NullPointerException
+        }
+
+        */
+
+
+//        app.obtainIllegalStateException();  // 5
+
+    }
+
 }
---
+++ homework01/src/Task03.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+public class Task03 {
+
+    public static void main(String[] args) {
+        Task03 app = new Task03();
+
+        int[] testArr1 = new int[]{90, 80, 70, 60, 50, 40, 30, 20, 10};
+        int[] testArr2 = Arrays.copyOf(testArr1, testArr1.length);
+
+        int[] testArr3 = Arrays.copyOfRange(testArr2, 0, testArr2.length - 1);
+
+        try {
+            System.out.println(Arrays.toString(app.divider(testArr1, testArr3)));  // incorrect
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            testArr2[4] = 0;        // let there will be `Infinity` in answer
+            System.out.println(Arrays.toString(app.divider(testArr1, testArr2)));    // correct
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    double[] divider(int[] arr1, int[] arr2) {
+        Logger log = Logger.getLogger(this.getClass().getSimpleName());
+
+        if (arr1.length != arr2.length) {
+            throw new RuntimeException("The arrays are of different length");
+
+        }
+        double[] result = new double[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            result[i] = (double) arr1[i] / arr2[i];
+        }
+        return result;
+    }
+
+}
